560c771ab80f2160a59da3a93e7c0a04ed4c1d0d
Giovana04 <126732272+Giovana04@users.noreply.github.com>
2024-07-21 14:03:18 -0300

    só atualização de documentação

--- Jogo da Velha/armazenamento/Historico.java
+++ Jogo da Velha/armazenamento/Historico.java
@@ -4,7 +4,18 @@ import java.io.*;
 import jogodavelha.Tabuleiro;
 
 /**
- * A classe Historico herda de Tabuleiro e gerencia o histórico dos tabuleiros em um arquivo txt.
+ * A classe Historico é a funcionalidade extra do código, ela mostra o histórico das ultimas partidas jogadas.
+ * 
+ * O Historico funciona de forma simples, todo fim de jogo, tenha ele terminado em vitória ou empate, é feito uma leitura do tabuleiro
+ * e cada um dos caracteres é armazenado em um arquivo txt. Esse arquivo pode ser acessado no menu do jogo "Histórico de partidas <2>".
+ * 
+ * A funcionalidade foi pensada com o intuito de permitir que os jogadores possam discutir o processo das partidas anteriores, análisar 
+ * as vitórias e os empates, estrátegias de jogo - mesmo que seja um jogo simples - ou só conversar sobre os resultados.
+ * 
+ * Sobre o processo de implementação: Nosso primeiro pensamento era fazer um vetor de matrizes que armazenariam um número limitado de jogos,
+ * após uma primeira tentativa decidimos nos inspirar no sistema de pontuação em txt e criamos outro arquivo textual para o histórico. Implementamos
+ * a forma de salvar o tabuleira a cada fim de jogo e em seguida como ele iria salvar continuamente os jogos, por fim só precisamos criar o print do txt.
+ *  
  * @author Giovana04
  * @author diegocamargo55555
  * @version 1.8.0_411
@@ -12,7 +23,7 @@ import jogodavelha.Tabuleiro;
 public class Historico extends Tabuleiro {
     
 /**
- * reseta o arquivo historico.txt.
+ * Reseta o arquivo txt.
  */
     public void resetHistoricoMapa() {
         try (FileWriter fw = new FileWriter("historico.txt")) {
